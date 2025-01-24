4e13120e40a606129bc275f5cac5035d443d97d1
rodi-314 <rodi314159@gmail.com>
2025-01-24 16:59:33 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Siri.java
@@ -0,0 +1,14 @@
+public class Siri {
+    public static void printHorizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        printHorizontalLine();
+        System.out.println("Hello! I'm Siri. " + "\uD83D\uDE0A");
+        System.out.println("What can I do for you?");
+        printHorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine();
+    }
+}
