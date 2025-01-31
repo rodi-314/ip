1f2112013e8833d2ba0b593ef2ca5b36d842b10f
rodi-314 <rodi314159@gmail.com>
2025-01-31 14:25:22 +0800

    Level 1. Echo

--- src/main/java/Siri.java
+++ src/main/java/Siri.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
+
 public class Siri {
     public static void printHorizontalLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
     }
 
     public static void main(String[] args) {
         printHorizontalLine();
-        System.out.println("Hello! I'm Siri. " + "\uD83D\uDE0A");
-        System.out.println("What can I do for you?");
+        System.out.println("    Hello! I'm Siri. " + "\uD83D\uDE0A");
+        System.out.println("    What can I do for you?");
         printHorizontalLine();
-        System.out.println("Bye. Hope to see you again soon!");
+
+        String line = "";
+        Scanner in = new Scanner(System.in);
+        while (!line.equals("bye")) {
+            line = in.nextLine();
+            printHorizontalLine();
+            if (!line.equals("bye")) {
+                System.out.println("    " + line);
+                printHorizontalLine();
+            }
+        }
+
+        System.out.println("    Bye. Hope to see you again soon!");
         printHorizontalLine();
     }
 }
